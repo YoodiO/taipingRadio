4d37bf22e8f9d9609e1bedb8b575eb23ac10bc9f
BuildTools <unconfigured@null.spigotmc.org>
2019-05-16 23:05:09 +0800

    新增:Cookie中设置班名、真实姓名。

--- tls/account.php
+++ tls/account.php
@@ -1,7 +1,9 @@
 <?php
 include( 'conn.php' );
 class account {
-    function reg( $username, $password, $realname, $classname ) {
+    private $realname;
+    private $classname;
+    static function reg( $username, $password, $realname, $classname ) {
         if ( $conn->query( "INSERT INTO users(username,password,realname,classname) VALUES($username,$password,$realname,$classname)" ) ) {
             return ( true );
         } else {
@@ -9,9 +11,12 @@ class account {
         }
     }
 
-    function login( $username, $password ) {
-        if ( $conn->query( "SELECT * FROM users WHERE username=$username AND password=$password" ) ) {
+    static function login( $username, $password ) {
+        if ( $res=$conn->query( "SELECT * FROM users WHERE username=$username AND password=$password" ) ) {
+            $row=$res->fetch_assoc;
             setcookie( 'username', $username, time() + 3600, "/", true );
+            setcookie( 'realname', $row['realname'], time() + 3600, "/", true );
+            setcookie( 'classname', $row['classname'], time() + 3600, "/", true );
             return ( true );
         } else {
             return( false );
